02adb46d1b619bb841fa85fb532bf8aaad39d202
tiagofer <tiago.fer1987@gmail.com>
2015-04-26 00:58:39 -0300

    versao 0.0.3 - Create Menu

--- KolksJam/Assets/Scripts/GamePlay/RespawnBalloon.cs
+++ KolksJam/Assets/Scripts/GamePlay/RespawnBalloon.cs
@@ -4,21 +4,38 @@ using System.Collections;
 public class RespawnBalloon : MonoBehaviour {
 	
 	public GameObject balloon;
-	public GameObject arrowRight;
-	public GameObject arrowLeft;
-	public GameObject arrowUp;
-	public GameObject arrowDown;
+	public Transform north;
+	public Transform south;
+	public Transform southEast;
+	public Transform centerEast;
+	public Transform northEast;
 
 	private float _tempoRespawn = 0f;
 	private float tempoCorrido = 0f;
+	private int region;
 	
 	// Update is called once per frame
 	void Update () {
 		tempoCorrido += Time.deltaTime;
 		_tempoRespawn = Random.Range (4f, 10f);
+		region = Random.Range (1, 6);
 		if (tempoCorrido >= _tempoRespawn) {
-			Instantiate(balloon, transform.position, balloon.transform.rotation);
-			tempoCorrido = 0f;
+			if(region == 1) {
+				Instantiate(balloon, north.position, balloon.transform.rotation);
+				tempoCorrido = 0f;
+			} else if (region == 2) {
+				Instantiate(balloon, south.position, balloon.transform.rotation);
+				tempoCorrido = 0f;
+			} else if (region == 3) {
+				Instantiate(balloon, southEast.position, balloon.transform.rotation);
+				tempoCorrido = 0f;
+			} else if (region == 4) {
+				Instantiate(balloon, centerEast.position, balloon.transform.rotation);
+				tempoCorrido = 0f;
+			} else if (region == 5) {
+				Instantiate(balloon, northEast.position, balloon.transform.rotation);
+				tempoCorrido = 0f;
+			}
 		}
 	}
 }
