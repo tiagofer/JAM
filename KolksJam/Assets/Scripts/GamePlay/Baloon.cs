6d602611f224217d789859da2fbc5902b4b0e868
tiagofer <tiago.fer1987@gmail.com>
2015-04-25 23:08:23 -0300

    versao 0.0.3 - Create Menu

---
+++ KolksJam/Assets/Scripts/GamePlay/Baloon.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Baloon : MonoBehaviour {
+
+	public GameObject obj;
+	private float timeDestroy;
+
+	// Use this for initialization
+	void Start () {
+		timeDestroy = Random.Range (1f, 4f);
+		Destroy (obj, timeDestroy);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ KolksJam/Assets/Scripts/GamePlay/RespawnBalloon.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RespawnBalloon : MonoBehaviour {
+	
+	public GameObject balloon;
+	public GameObject arrowRight;
+	public GameObject arrowLeft;
+	public GameObject arrowUp;
+	public GameObject arrowDown;
+
+	private float _tempoRespawn = 0f;
+	private float tempoCorrido = 0f;
+	
+	// Update is called once per frame
+	void Update () {
+		tempoCorrido += Time.deltaTime;
+		_tempoRespawn = Random.Range (4f, 10f);
+		if (tempoCorrido >= _tempoRespawn) {
+			Instantiate(balloon, transform.position, balloon.transform.rotation);
+			tempoCorrido = 0f;
+		}
+	}
+}
